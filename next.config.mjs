85fdb78f00d63755c9290b1a6b058fa8c992fe09
Aman Kumar <73690598+aman01k@users.noreply.github.com>
2024-10-01 23:16:05 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
         port: "",
       },
       {
-        hostname: "glvmmupiqwlmhicmggqp.supabase.co",
+        hostname: "eensqzgsmxkneygkkxae.supabase.co",
         protocol: "https",
         port: "",
       },
